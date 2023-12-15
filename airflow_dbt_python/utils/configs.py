360472c80567295a2d384b782fbb3e089bda864e
Denny Riadi <den.riadi@gmail.com>
2023-12-15 23:12:11 +1300

    remove printer_width

--- airflow_dbt_python/utils/configs.py
+++ airflow_dbt_python/utils/configs.py
@@ -124,7 +124,7 @@ class BaseConfig:
     debug: Optional[bool] = None
     quiet: Optional[bool] = None
     no_print: Optional[bool] = None
-    printer_width: Optional[int] = None
+
 
     # Mutually exclusive attributes
     defer: Optional[bool] = None
